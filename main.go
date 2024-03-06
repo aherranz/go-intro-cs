892d9689729564cee698b05004140694c630fe14
Ángel Herranz <aherranz@fi.upm.es>
2024-03-06 19:26:33 +0100

    Pasos 3-6

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	const serverAddr string = "127.0.0.1:4000"
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("HTTP Caracola"))
+		http.ListenAndServe(serverAddr, nil)
+	})
 	fmt.Println("Hola Caracola")
 }
