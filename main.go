5c386d4d93d58629fe20d4eb0040978e4db15d8d
Ángel Herranz <aherranz@fi.upm.es>
2024-03-06 19:45:10 +0100

    Terminada sección de deserialización.

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import "fmt"
+import "net/http"
+import "encoding/json"
+
+type Scope struct {
+        Project string
+        Area    string
+}
+
+type Note struct {
+        Title string
+        Tags  []string
+        Text  string
+        Scope Scope
+}
+
+func createNote(w http.ResponseWriter, r *http.Request) {
+	var note Note
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&note); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Note: %+v", note)
+}
 
 func main() {
-	const serverAddr string = "127.0.0.1:4000"
+	const serverAddr string = "127.0.0.1:8081"
+
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("HTTP Caracola"))
-		http.ListenAndServe(serverAddr, nil)
+		w.Write([]byte("HTTP Caracola\n"))
 	})
+
+	http.HandleFunc("POST /", createNote)
+	
+	http.ListenAndServe(serverAddr, nil)
 	fmt.Println("Hola Caracola")
 }
